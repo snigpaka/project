58527b2c7b317a1a4a80305d5caf3ca5ed21c513
snigpaka <snigpaka@users.noreply.github.com>
2015-09-27 01:03:14 -0400

    able to print date below time

--- src/main.c
+++ src/main.c
@@ -5,17 +5,17 @@ static TextLayer *s_time_layer;
 static GFont s_time_font;
 //static BitmapLayer *s_background_layer;
 //static GBitmap *s_background_bitmap;
-
+static GFont s_date_font;
 static TextLayer *s_date_layer;
 
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-  struct tm *date;
+
   // Create a long-lived buffer
   static char buffer[] = "00:00";
-  static char datebuffer[] = "00/00/0000";
+  static char datebuffer[] = "00/00/00";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
@@ -26,7 +26,7 @@ static void update_time() {
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
   
-  strftime(datebuffer, sizeof("00/00/0000"), "%h/%d/%Y", date);
+  strftime(datebuffer, sizeof("00/00/00"), "%m/%d/%y", tick_time);
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
   text_layer_set_text(s_date_layer, datebuffer);
@@ -39,30 +39,30 @@ static void main_window_load(Window *window) {
   //layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
-  s_date_layer = text_layer_create(GRect(5, 52, 139, 50));
+  s_time_layer = text_layer_create(GRect(20, 30, 100, 80));
+  s_date_layer = text_layer_create(GRect(1, 80, 143, 167));
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_48));
-
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_35));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_24));
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  
-  text_layer_set_font(s_date_layer, s_time_font);
+
+  text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorBlack);
   text_layer_set_text(s_date_layer, "00/00/00");
-
+  
   // Improve the layout to be more like a watchface
   //text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_48));
 
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
   // Make sure the time is displayed from the start
   update_time();
 }
@@ -78,8 +78,8 @@ static void main_window_unload(Window *window) {
   fonts_unload_custom_font(s_time_font);
 
   // Destroy TextLayer
-  text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
+  text_layer_destroy(s_time_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
